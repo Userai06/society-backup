7b3e281494794b9f304f01c066db1735ad014b5a
Userai06 <mseahr25@gmail.com>
2025-11-08 23:34:18 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { AuthProvider, useAuth } from './contexts/AuthContext';
 import { ThemeProvider } from './contexts/ThemeContext';
 import Navbar from './components/Navbar';
 import Login from './components/Login';
+import WelcomeAnimation from './components/WelcomeAnimation';
 import SeniorDashboard from './components/Dashboard/SeniorDashboard';
 import JuniorDashboard from './components/Dashboard/JuniorDashboard';
 import TaskManager from './components/Tasks/TaskManager';
@@ -29,6 +30,7 @@ const AppContent: React.FC = () => {
   return (
     <ThemeProvider>
       <Router>
+        <WelcomeAnimation />
         <div className="min-h-screen bg-gray-900 dark:bg-gray-900 bg-gray-50 transition-colors duration-300">
           <Navbar />
           <main>
